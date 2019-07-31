49b17d4df0195b65683684f68a8fdf993369459d
knutmerket <dodraugen2k@hotmail.com>
2019-07-31 19:32:20 +0200

    once again, the settings...

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=vz1bx3vge!0g)co0pe#(jch*b!j8qon)hsrz_r55qigyp!d8_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
